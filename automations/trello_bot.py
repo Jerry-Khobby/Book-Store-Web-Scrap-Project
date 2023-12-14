891f2d70ced176066d68db83fc67ed8b07fd33dc
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-12-13 13:19:42 -1200

    the first click is working

--- automations/trello_bot.py
+++ automations/trello_bot.py
@@ -1,7 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from datetime import date
+import os
+import json
 
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options = option)
 
-driver.get('https://www.google.com/')
+
+
+
+
+
+
+# defining the function to enable me login 
+def login():
+    with open("config.json") as configFile:
+        credentials=json.load(configFile)
+        time.sleep(2)
+        driver.find_element(By.XPATH, value="//a[text()='Log in']").click()
+        time.sleep(2)
+        
+
+
+#I will have to define a main function to take care of my other functions for me 
+def main():
+    try:
+        driver.get('https://trello.com/home')
+        login()
+        driver.close()
+    except Exception as e:
+        print(e)
+        driver.close()
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
