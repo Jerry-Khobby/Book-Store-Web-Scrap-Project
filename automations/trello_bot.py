a1deb29d2ac78156fe0c3d3e040460b9b6eb8374
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-12-13 14:34:23 -1200

    done with the login feature

--- automations/trello_bot.py
+++ automations/trello_bot.py
@@ -19,12 +19,30 @@ driver = webdriver.Chrome(options = option)
 
 # defining the function to enable me login 
 def login():
-    with open("config.json") as configFile:
-        credentials=json.load(configFile)
+    with open('config.json') as configFile:
+        credentials = json.load(configFile)
         time.sleep(2)
-        driver.find_element(By.XPATH, value="//a[text()='Log in']").click()
+        driver.find_element(By.XPATH,value="//a[text()='Log in']").click()
         time.sleep(2)
+        username_input = driver.find_element(By.CSS_SELECTOR, "#username")
+        username_input.send_keys(credentials['USERNAME'])
+        time.sleep(5)
+        # Locate and click the "Continue" button using its CSS selector
+        driver.find_element(By.CSS_SELECTOR, "#login-submit").click()
+        time.sleep(5)
+        #I want to locate the password field 
+        password_input = driver.find_element(By.CSS_SELECTOR, "#password")
+        password_input.send_keys(credentials['PASSWORD'])  #
+        time.sleep(5)
+        #finally I will be smashing it on that login button 
+         # Locate and click the "Log in" button using its CSS selector
+        login_button = driver.find_element(By.CSS_SELECTOR, "#login-submit")
+        login_button.click()
+        time.sleep(20)
+
+
         
+
 
 
 #I will have to define a main function to take care of my other functions for me 
